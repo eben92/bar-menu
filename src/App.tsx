03c462ada8bb1aa7f43126b1170471f7d8265590
rhutmann <pappy.rhutmannjnrolege@gmail.com>
2024-01-28 12:44:31 +0000

    add pdf viewer

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Viewer, SpecialZoomLevel } from "@react-pdf-viewer/core";
+import "./App.css";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+
+import pdf from "./assets/menu.pdf?url";
+
+function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <Viewer
+        theme={"dark"}
+        fileUrl={pdf}
+        defaultScale={SpecialZoomLevel.PageFit}
+      />
+    </div>
+  );
+}
+
+export default App;
